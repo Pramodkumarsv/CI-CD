cb24f3fd5760b3a9e3dafdb04570408c721466c8
Pramodkumarsv <svpramodkumar1@gmail.com>
2025-04-02 15:59:25 +0530

    Update demoTest.java

--- src/test/java/First_project/demoTest.java
+++ src/test/java/First_project/demoTest.java
@@ -1,14 +1,12 @@
-package First_project;
-
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class demoTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    public void testAddition() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        assertEquals(15, sum);
+    }
 }
